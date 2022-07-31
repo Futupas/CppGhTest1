a7e81cb993a7e0c0fc824c00157057627a9a187b
Olexandr Pascal <futupas@gmail.com>
2022-07-31 18:04:21 +0000

    upd: returning by links

--- main.cpp
+++ main.cpp
@@ -7,23 +7,55 @@ class A
         int Var;
 };
 
-int DoSomething(A& a) 
+A DoSomething1(A a) 
 {
-    std::cout << "&a (inside DoSomething): " << &a << "\n";
-    a.Var = 5;
-    return 0;
+    std::cout << "a (DoSomething1): " << &a << "\n";
+    return a;
+}
+A& DoSomething2(A a) 
+{
+    std::cout << "a (DoSomething2): " << &a << "\n";
+    return a;
+}
+A DoSomething3(A& a) 
+{
+    std::cout << "a (DoSomething3): " << &a << "\n";
+    return a;
+}
+A& DoSomething4(A& a) 
+{
+    std::cout << "a (DoSomething4): " << &a << "\n";
+    return a;
 }
 
 
 int main() {
-    std::cout << "Hello worl1\n";
+    std::cout << "Hello worl2\n";
 
     A a = A();
-    std::cout << a.Var << "\n";
-    a.Var = 9;
-    std::cout << &a << a.Var << "\n";
-    DoSomething(a);
-    std::cout << a.Var << "\n";
+    std::cout << "a (main): " << &a << "\n\n";
+
+    A a1 = DoSomething1(a);
+    std::cout << "a (after 1): " << &a1 << "\n\n";
+
+    // A a2 = DoSomething2(a);
+    // std::cout << "a (after 2): " << &a2 << "\n\n";
+    // // Segmentation fault (core dumped)
+    
+    A a3 = DoSomething3(a);
+    std::cout << "a (after 3): " << &a3 << "\n\n";
+    
+    A a4 = DoSomething4(a);
+    std::cout << "a (after 4): " << &a4 << "\n\n";
+    
+    A& a5 = DoSomething4(a);
+    std::cout << "a (after 4, but link): " << &a5 << "\n\n";
+    
+    A& a6 = a;
+    std::cout << "a (A& a6 = a): " << &a6 << "\n\n";
+    
+    A a7 = a;
+    std::cout << "a (A a7 = a): " << &a7 << "\n\n";
 
 
     return 0;
